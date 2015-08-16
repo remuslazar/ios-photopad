1909fb4fffa0e8916ea83d7f782313a61a4ce4e1
Remus Lazar <remus@lazar.info>
2015-08-17 00:54:06 +0200

    first working version

--- Photopad/PhotoViewController.swift
+++ Photopad/PhotoViewController.swift
@@ -7,28 +7,133 @@
 //
 
 import UIKit
+import AVFoundation
 
-class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
 
     // MARK: - public API
     var image: UIImage? {
         didSet {
-            imageView?.image = image
-        }
-    }
-    
-    // MARK: - Outlets
-    @IBOutlet weak var imageView: UIImageView! {
-        didSet {
-            if image != nil {
-                imageView.image = image
+            if imageSizeControl.selectedSegmentIndex != 0 {
+                imageSizeControl.selectedSegmentIndex = 0
             }
         }
     }
     
+    private var convertedImage: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            imageView.frame.origin = CGPointZero
+            scrollView.contentSize = imageView.frame.size
+//            println("imageView frame: \(imageView.frame)")
+            fitImage()
+            title = ""
+            if let imageData = UIImageJPEGRepresentation(imageView.image, 0.75) {
+                let fileSizeString = NSByteCountFormatter.stringFromByteCount(Int64(imageData.length), countStyle: NSByteCountFormatterCountStyle.Binary)
+                title = "\(fileSizeString) \(Int(imageView.image!.size.width))x\(Int(imageView.image!.size.height))"
+            }
+        }
+    }
+    
+    private func fitImage() {
+        if let image = convertedImage {
+            let rect = AVMakeRectWithAspectRatioInsideRect(image.size, scrollView.bounds)
+            let scale = rect.size.width / image.size.width
+            scrollView.zoomScale = scale
+            scrollView.minimumZoomScale = scale
+            scrollView.maximumZoomScale = max(1.0, scale)
+        }
+    }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    @IBAction func selectAction(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "Source Image", message: "Select Input Image Source", preferredStyle: .ActionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .Default, handler: { [unowned self] (_) in
+                self.takePhoto(self)
+                }))
+        }
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera Roll", style: .Default, handler: { [unowned self] (_) in
+            self.pickPhoto()
+            }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(actionSheet, animated: true, completion: nil)
+
+    }
+    
+    @IBAction func saveImage(sender: AnyObject) {
+        if let image = convertedImage {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+            saveButton.enabled = false
+        }
+    }
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    private func pickPhoto() {
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .PhotoLibrary
+            picker.delegate = self
+            presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    private var imageView = UIImageView()
+    
+    private func resizeImage(maxSize: CGSize) {
+        if let originalImage = image {
+            convertedImage = nil
+            spinner.startAnimating()
+            dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0), { [unowned self] in
+                let rect = AVMakeRectWithAspectRatioInsideRect(originalImage.size, CGRect(origin: CGPointZero, size: maxSize))
+                let scale = rect.size.width / originalImage.size.width
+                let image = CIImage(image: originalImage)
+                let filter = CIFilter(name: "CILanczosScaleTransform")
+                filter.setValue(image, forKey: "inputImage")
+                filter.setValue(scale, forKey: "inputScale")
+                filter.setValue(1.0, forKey: "inputAspectRatio")
+                if let outputImage = filter.valueForKey("outputImage") as? CIImage {
+                    let context = CIContext(options: nil)
+                    let scaledImage = UIImage(CGImage: context.createCGImage(outputImage, fromRect: outputImage.extent()))
+                    dispatch_async(dispatch_get_main_queue(), { [unowned self] in
+                        self.spinner.stopAnimating()
+                        self.convertedImage = scaledImage
+                    })
+                }
+            })
+        }
+    }
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+        }
+    }
+    
+    @IBOutlet weak var imageSizeControl: UISegmentedControl!
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
     @IBAction func exportImage(sender: UISegmentedControl) {
-        if let image = image {
-            
+        saveButton.enabled = true
+        switch sender.selectedSegmentIndex {
+        case 1: resizeImage(CGSize(width: 1280, height: 1280))
+        case 2: resizeImage(CGSize(width: 800, height: 800))
+        case 3: resizeImage(CGSize(width: 480, height: 480))
+        default: convertedImage = image
         }
     }
     
@@ -65,15 +170,17 @@ class PhotoViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if image == nil {
-            takePhoto(self)
-        }
-        
-        // just a quick workaround for being able to use the simulator
-        if image == nil {
-            image = UIImage(named: "DummyImage")
-        }
+        scrollView.addSubview(imageView)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        convertedImage = image
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        fitImage()
     }
 
     override func didReceiveMemoryWarning() {
